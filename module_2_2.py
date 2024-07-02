cf57da5e2f0da8328d8041d9973feef15cb8d7bd
Highway07 <vitalijodnobokov@gmail.com>
2024-07-02 18:26:24 +0300

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,9 @@
+first = input('First int:')
+second = input('Second int:')
+third = input('Third int:')
+if first == second == third:
+    print('количество одинаковых чисел равно 3')
+elf first == second or first == third or second == third:
+    print('оличество одинаковых чисел равно 2')
+else:
+print('количуство одинаковых чисел равно 0')
